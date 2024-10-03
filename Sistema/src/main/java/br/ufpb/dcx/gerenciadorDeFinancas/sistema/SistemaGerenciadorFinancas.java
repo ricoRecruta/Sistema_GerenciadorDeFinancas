59046ef9bc96e770889b385bf7c6a82456edab5a
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-10-02 23:34:18 -0300

    Alterando os parâmetros dos métodos e adicionando as exceções na interface SistemaGerenciadorFinancas

--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/sistema/SistemaGerenciadorFinancas.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/sistema/SistemaGerenciadorFinancas.java
@@ -1,15 +1,18 @@
 package br.ufpb.dcx.gerenciadorDeFinancas.sistema;
 
-import br.ufpb.dcx.gerenciadorDeFinancas.login.Conta;
+import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.*;
 
 import java.util.Collection;
+
 public interface SistemaGerenciadorFinancas {
-public void cadastrarSalario(double salario);
-public Collection<Compra> pesquisarPorCategoria(categoriaCompra categoria);
-public void cadastrarCompra(Compra compra);
-public void editarSalario(double novoSalario);
-public double exibirTotalGasto();
-public void removerCompra(String descricao);
-public void editarCompra(String descricao,categoriaCompra novaCategoria, double novoValor);
-public Collection<Compra> comparacaoDeGastos(double gasto1, double gasto2);
+    public void cadastrarSalario(double salario);
+    public void editarSalario(double novoSalario);
+
+    public void cadastrarCompra(Compra compra) throws CompraJaCadastradaException;
+    public void editarCompra(Chave idCompra, categoriaCompra novaCategoria, double novoValor, String novaDescricao) throws CompraNaoExisteException;
+    public void removerCompra(Compra compra) throws CompraNaoExisteException;
+    public Collection<Compra> pesquisarPorCategoria(categoriaCompra categoria);
+
+    public double exibirTotalGasto();
+    public Collection<Compra> comparacaoDeGastos(double gasto1, double gasto2);
 }
