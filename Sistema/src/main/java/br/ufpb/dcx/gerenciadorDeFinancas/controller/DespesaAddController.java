4a6586cbe5e86794fe32240d77bef1d0324a7d93
Fernando <fernandoribeirogomesjunior@gmail.com>
2024-10-04 20:41:22 -0300

    Implementando controllers DespesaAddController, DespesaEditController

--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaAddController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaAddController.java
@@ -1,11 +1,48 @@
 package br.ufpb.dcx.gerenciadorDeFinancas.controller;
 
+import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.DespesaJaCadastradaException;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.CategoriaDespesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.Despesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.SistemaFinancas;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DespesaAddController implements ActionListener {
+
+    private SistemaFinancas financas;
+    private JFrame janelaPrincipal;
+
+    public DespesaAddController(SistemaFinancas financas, JFrame janela){
+        this.financas = financas;
+        this.janelaPrincipal = janela;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        try {
+            String id = JOptionPane.showInputDialog(janelaPrincipal, "Digite o ID da Despesa: " );
+            CategoriaDespesa categoria = (CategoriaDespesa) JOptionPane.showInputDialog(janelaPrincipal,"Selecione a categoria:","Categoria", JOptionPane.QUESTION_MESSAGE, null, CategoriaDespesa.values(), CategoriaDespesa.values()[0]);
+            String valorStr = JOptionPane.showInputDialog(janelaPrincipal,"Digite o valor da despesa: ");
+            Double valor = Double.parseDouble(valorStr);
+            String descricao = JOptionPane.showInputDialog(janelaPrincipal, "Digite a Descrição da Despesa:");
+            String dataStr = JOptionPane.showInputDialog(janelaPrincipal, "Informe a data da Despesa: Modelo = yyy-MM-dd ");
+            LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            Despesa novaDespesa = new Despesa(id, categoria, valor, descricao, data);
+
+            financas.cadastrarDespesa(novaDespesa);
+            JOptionPane.showMessageDialog(janelaPrincipal, "Despesa cadastrada com Sucesso!");
+
+        }catch (DespesaJaCadastradaException ex){
+            JOptionPane.showMessageDialog(janelaPrincipal,ex.getMessage());
+        }catch (IllegalArgumentException ex){
+            JOptionPane.showMessageDialog(janelaPrincipal, "Data Inválida. Modelo: (yyyy-MM-dd)");
+        }
+
 
     }
 }
--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaEditController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaEditController.java
@@ -1,4 +1,44 @@
 package br.ufpb.dcx.gerenciadorDeFinancas.controller;
 
-public class DespesaEditController {
+import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.DespesaNaoExisteException;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.CategoriaDespesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.Despesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.SistemaFinancas;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DespesaEditController implements ActionListener {
+
+    private SistemaFinancas financas;
+    private JFrame janelaPrincipal;
+
+    public DespesaEditController(SistemaFinancas financas, JFrame janela) {
+        this.financas = financas;
+        this.janelaPrincipal = janela;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            String id = JOptionPane.showInputDialog(janelaPrincipal, "Digite o ID da Despesa à ser editada: ");
+            CategoriaDespesa categoria = (CategoriaDespesa) JOptionPane.showInputDialog(janelaPrincipal, "Selecione a nova categoria:", "Categoria", JOptionPane.QUESTION_MESSAGE, null, CategoriaDespesa.values(), CategoriaDespesa.values()[0]);
+            String valorStr = JOptionPane.showInputDialog(janelaPrincipal, "Digite o novo valor da despesa: ");
+            Double novoValor = Double.parseDouble(valorStr);
+            String descricao = JOptionPane.showInputDialog(janelaPrincipal, "Digite a nova Descrição da Despesa:");
+            String dataStr = JOptionPane.showInputDialog(janelaPrincipal, "Informe a  nova data da Despesa: Modelo = yyy-MM-dd ");
+            LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            financas.editarDespesa(id, categoria, novoValor, descricao, data);
+            JOptionPane.showMessageDialog(janelaPrincipal, "Despesa editada com sucesso!");
+        } catch (DespesaNaoExisteException ex) {
+            JOptionPane.showMessageDialog(janelaPrincipal, ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            JOptionPane.showMessageDialog(janelaPrincipal, "Data Inválida. Modelo: (yyy-MM-dd)");
+        }
+    }
 }
+
