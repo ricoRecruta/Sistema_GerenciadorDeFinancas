2f789bb305c7f349dbaf66bf9b62c117f0ba4c42
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-10-05 16:24:54 -0300

    Implementando o JFrame na classe ReceitaRemoveController

--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/ReceitaRemoveController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/ReceitaRemoveController.java
@@ -1,4 +1,22 @@
 package br.ufpb.dcx.gerenciadorDeFinancas.controller;
 
-public class ReceitaRemoveController {
+import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.ReceitaNaoExisteException;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.SistemaGerenciadorFinancas;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ReceitaRemoveController implements ActionListener {
+    private SistemaGerenciadorFinancas sistema;
+    private JFrame janela;
+
+    public ReceitaRemoveController(SistemaGerenciadorFinancas sistema, JFrame janela){
+        this.sistema = sistema;
+        this.janela = janela;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
