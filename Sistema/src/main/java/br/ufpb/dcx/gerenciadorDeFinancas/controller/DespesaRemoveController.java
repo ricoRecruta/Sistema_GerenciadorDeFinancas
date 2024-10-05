32337eb239484024af62d6e1e29fdfe84e9fb716
Fernando <fernandoribeirogomesjunior@gmail.com>
2024-10-05 00:41:13 -0300

    Implementado DespesaRemoveController, DespesaSearchController

--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaRemoveController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaRemoveController.java
@@ -6,25 +6,34 @@ import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DespesaRemoveController implements ActionListener {
-    private SistemaGerenciadorFinancas sistema;
+    private SistemaGerenciadorFinancas financas;
+    private JFrame janelaPrincipal;
 
-    public DespesaRemoveController(SistemaGerenciadorFinancas sistema) {
-        this.sistema = sistema;
+    public DespesaRemoveController(SistemaGerenciadorFinancas financas, JFrame janela) {
+        this.financas = financas;
+        this.janelaPrincipal = janela;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String id = JOptionPane.showInputDialog(null, "Informe o ID para remover:");
-
         try {
-            sistema.removerDespesa(new Despesa(id, null, 0.0, null, null));
-            JOptionPane.showMessageDialog(null, "Despesa removida com sucesso!");
+            String id = JOptionPane.showInputDialog(janelaPrincipal, "Informe o ID para remover:");
+            CategoriaDespesa categoria = (CategoriaDespesa) JOptionPane.showInputDialog(janelaPrincipal, "Selecione a categoria para remover:", "Categoria", JOptionPane.QUESTION_MESSAGE, null, CategoriaDespesa.values(), CategoriaDespesa.values()[0]);
+            String valorStr = JOptionPane.showInputDialog(janelaPrincipal, "Digite o valor da despesa à remover: ");
+            Double valor = Double.parseDouble(valorStr);
+            String descricao = JOptionPane.showInputDialog(janelaPrincipal, "Digite a Descrição da Despesa para remover:");
+            String dataStr = JOptionPane.showInputDialog(janelaPrincipal, "Informe a data da Despesa para remover: Modelo = yyyy-MM-dd ");
+            LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            financas.removerDespesa(new Despesa(id, categoria, valor, descricao, data));
+            JOptionPane.showMessageDialog(janelaPrincipal, "Despesa removida com sucesso!");
         } catch (DespesaNaoExisteException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(janelaPrincipal, ex.getMessage());
+        }catch (IllegalArgumentException ex) {
+            JOptionPane.showMessageDialog(janelaPrincipal, "Data Inválida. Modelo: (yyy-MM-dd)");
         }
     }
-
-
 }
--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaSearchController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaSearchController.java
@@ -1,11 +1,36 @@
 package br.ufpb.dcx.gerenciadorDeFinancas.controller;
 
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.CategoriaDespesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.Despesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.SistemaFinancas;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.SistemaGerenciadorFinancas;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
 
 public class DespesaSearchController implements ActionListener {
+
+    private SistemaGerenciadorFinancas financas;
+    private JFrame janelaPrincipal;
+
+    public DespesaSearchController(SistemaGerenciadorFinancas financas, JFrame janela){
+        this.financas = financas;
+        this.janelaPrincipal = janela;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        CategoriaDespesa categoria = (CategoriaDespesa) JOptionPane.showInputDialog(janelaPrincipal,"Selecione a categoria","Categoria", JOptionPane.QUESTION_MESSAGE, null, CategoriaDespesa.values(), CategoriaDespesa.values()[0]);
+        Collection<Despesa> despesasEncontradas = financas.pesquisarPorCategoria(categoria);
+
+        if (despesasEncontradas.isEmpty()){
+            JOptionPane.showMessageDialog(janelaPrincipal, "Nenhuma despesa foi encontrada para a categoria: " + categoria);
+        }else {
+            JOptionPane.showMessageDialog(janelaPrincipal,"Despesa(s) encontrada(s) com sucesso!");
+        }
+
 
     }
 }
