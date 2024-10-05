51b812bbaed7043da4edf9b8e6edbe875e55eaf0
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-10-05 17:39:28 -0300

    Implementando a lógica no metódo para relizar a ação de remover na classe ReceitaRemoveController

--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/ReceitaRemoveController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/ReceitaRemoveController.java
@@ -1,11 +1,17 @@
 package br.ufpb.dcx.gerenciadorDeFinancas.controller;
 
+import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.DespesaNaoExisteException;
 import br.ufpb.dcx.gerenciadorDeFinancas.exceptions.ReceitaNaoExisteException;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.CategoriaDespesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.Despesa;
+import br.ufpb.dcx.gerenciadorDeFinancas.sistema.Receita;
 import br.ufpb.dcx.gerenciadorDeFinancas.sistema.SistemaGerenciadorFinancas;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ReceitaRemoveController implements ActionListener {
     private SistemaGerenciadorFinancas sistema;
@@ -17,6 +23,18 @@ public class ReceitaRemoveController implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        try {
+            String id = JOptionPane.showInputDialog(janela, "Informe o ID para remover:");
+            String valorStr = JOptionPane.showInputDialog(janela, "Digite o valor da despesa à remover: ");
+            double valor = Double.parseDouble(valorStr);
+            String dataStr = JOptionPane.showInputDialog(janela, "Informe a data da Despesa para remover: Modelo = yyyy-MM-dd ");
+            LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            sistema.removerReceita(new Receita(id, valor, data));
+            JOptionPane.showMessageDialog(janela, "Despesa removida com sucesso!");
+        } catch (ReceitaNaoExisteException ex) {
+            JOptionPane.showMessageDialog(janela, ex.getMessage());
+        }catch (IllegalArgumentException ex) {
+            JOptionPane.showMessageDialog(janela, "Data Inválida. Modelo: (yyy-MM-dd)");
+        }
     }
 }
