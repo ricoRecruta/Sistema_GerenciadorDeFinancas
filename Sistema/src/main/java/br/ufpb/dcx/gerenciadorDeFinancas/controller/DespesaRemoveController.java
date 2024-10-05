f4fc9b1d8c9e96b0a0dd0723703ba012a70916fd
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-10-05 17:37:15 -0300

    Alterando a variável financas para sistema e alterando o tipo da variável Double para um tipo primitivo

--- Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaRemoveController.java
+++ Sistema/src/main/java/br/ufpb/dcx/gerenciadorDeFinancas/controller/DespesaRemoveController.java
@@ -10,11 +10,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DespesaRemoveController implements ActionListener {
-    private SistemaGerenciadorFinancas financas;
+    private SistemaGerenciadorFinancas sistema;
     private JFrame janelaPrincipal;
 
-    public DespesaRemoveController(SistemaGerenciadorFinancas financas, JFrame janela) {
-        this.financas = financas;
+    public DespesaRemoveController(SistemaGerenciadorFinancas sistema, JFrame janela) {
+        this.sistema = sistema;
         this.janelaPrincipal = janela;
     }
 
@@ -24,11 +24,11 @@ public class DespesaRemoveController implements ActionListener {
             String id = JOptionPane.showInputDialog(janelaPrincipal, "Informe o ID para remover:");
             CategoriaDespesa categoria = (CategoriaDespesa) JOptionPane.showInputDialog(janelaPrincipal, "Selecione a categoria para remover:", "Categoria", JOptionPane.QUESTION_MESSAGE, null, CategoriaDespesa.values(), CategoriaDespesa.values()[0]);
             String valorStr = JOptionPane.showInputDialog(janelaPrincipal, "Digite o valor da despesa à remover: ");
-            Double valor = Double.parseDouble(valorStr);
-            String descricao = JOptionPane.showInputDialog(janelaPrincipal, "Digite a Descrição da Despesa para remover:");
-            String dataStr = JOptionPane.showInputDialog(janelaPrincipal, "Informe a data da Despesa para remover: Modelo = yyyy-MM-dd ");
+            double valor = Double.parseDouble(valorStr);
+            String descricao = JOptionPane.showInputDialog(janelaPrincipal, "Digite a descrição da Despesa para remover:");
+            String dataStr = JOptionPane.showInputDialog(janelaPrincipal, "Informe a data da despesa para remover: Modelo = yyyy-MM-dd ");
             LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            financas.removerDespesa(new Despesa(id, categoria, valor, descricao, data));
+            sistema.removerDespesa(new Despesa(id, categoria, valor, descricao, data));
             JOptionPane.showMessageDialog(janelaPrincipal, "Despesa removida com sucesso!");
         } catch (DespesaNaoExisteException ex) {
             JOptionPane.showMessageDialog(janelaPrincipal, ex.getMessage());
